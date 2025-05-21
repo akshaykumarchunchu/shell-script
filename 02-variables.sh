41b35600429abbbed67664a1b036a8c0bbee73ea
akshaykumarchunchu <kumarchunchu.1994@gmail.com>
2025-05-21 19:12:42 -0400

    some-message

--- 02-variables.sh
+++ 02-variables.sh
@@ -1,1 +1,7 @@
-This is also a sample script
+#!/bin/bash
+
+echo "Ramesh: How are you Suresh"
+echo "Suresh: i'm good Ramesh, How are you?"
+echo "Ramesh: im good too, How is your work?"
+echo "Suresh: not bad. I am thinking to upgrade to Devops"
+ 
