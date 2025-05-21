7eeb37253361aba75392f21497ac0cedc22e9dcc
akshaykumarchunchu <kumarchunchu.1994@gmail.com>
2025-05-21 19:29:11 -0400

    some message

--- 03-variables.sh
+++ 03-variables.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-PERSON1=Ramesh
+#declaring of variables
+
+PERSON1=Ramesh      #no space between = and value
 PERSON2=Suresh
+
+#refering variables
 
 echo "$PERSON1: How are you $PERSON2"
 echo "$PERSON2: i'm good $PERSON1, How are you?"
