66ba63fec01be731e9491975e8f9ea5ed2573f57
akshaykumarchunchu <kumarchunchu.1994@gmail.com>
2025-05-21 19:25:16 -0400

    some-message

---
+++ 03-variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+PERSON1=Ramesh
+PERSON2=Suresh
+
+echo "$PERSON1: How are you $PERSON2"
+echo "$PERSON2: i'm good $PERSON1, How are you?"
+echo "$PERSON1: im good too, How is your work?"
+echo "$PERSON2: not bad. I am thinking to upgrade to Devops"
+ 
