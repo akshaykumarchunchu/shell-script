2a8c4520237404afe7211b8beb535795687fffaa
akshaykumarchunchu <kumarchunchu.1994@gmail.com>
2025-05-21 19:50:51 -0400

    some message

---
+++ 04-variables.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#declaring of variables
+
+PERSON1=$1      #no space between = and value
+PERSON2=$2
+
+#refering variables
+echo "$PERSON1: How are you $PERSON2"
+echo "$PERSON2: i'm good $PERSON1, How are you?"
+echo "$PERSON1: im good too, How is your work?"
+echo "$PERSON2: not bad. I am thinking to upgrade to Devops"
+ 
