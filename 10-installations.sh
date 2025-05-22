e70deb46e533c57160941bf3f2c366d232c64b52
akshaykumarchunchu <kumarchunchu.1994@gmail.com>
2025-05-22 17:22:01 -0400

    some message

--- 10-installations.sh
+++ 10-installations.sh
@@ -7,8 +7,28 @@ USERID=$(id -u)
 if [ $USERID -ne 0 ]
 then 
     echo "Please enter as run with super user"
+    exit 1 # manually exit if error occures
 else 
     echo "You're super user"
 fi 
 
 dnf install mysql -y
+
+if [ $? -ne 0 ]
+then
+    echo "Installation of mysql is failure"
+    exit 1
+else    
+    echo "Installation of mysql is success"
+fi 
+
+dnf install git -y
+
+if [ $? -ne 0 ]
+then   
+    echo "Installation of git is failure"
+    exit 1
+else 
+    echo "installation of git is success"
+fi 
+
