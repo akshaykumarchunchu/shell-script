072c1f41351c4ffd4f577b82b76ed02bf74762df
akshaykumarchunchu <kumarchunchu.1994@gmail.com>
2025-05-28 13:43:58 -0400

    repeat

--- 06-array.sh
+++ 06-array.sh
@@ -5,7 +5,7 @@ Movies=("RRR" "Salaar" "Kalki")
 #list of index always starts with 0
 #size of expression is 3
 
-echo "First movie is ${Movies[0]}" #Movies means Array and [0] mens Index
+echo "First movie is ${Movies[0]}" #Movies means Array and [0] means Index
 echo "First movie is ${Movies[1]}"
 echo "First movie is ${Movies[@]}"
 
--- 11-repeat.sh
+++ 11-repeat.sh
@@ -2,7 +2,7 @@
 
 NAME=("Akshay" "Alekhya" "Anu")
 
-echo "NAME in List is ${NAME[0]}" #NAME means Array and [0] mens Index
+echo "NAME in List is ${NAME[0]}" #NAME means Array and [0] means Index
 echo "NAME in List is ${NAME[1]}"
 echo "NAME in List is ${NAME[@]}"
 
