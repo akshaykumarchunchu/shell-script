b94d75d1e9392b21580e1ab480c88d64ea65d92f
akshaykumarchunchu <kumarchunchu.1994@gmail.com>
2025-05-28 17:27:27 -0400

    some message

--- 11-repeat.sh
+++ 11-repeat.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
-NAME=("Akshay" "Alekhya" "Anu")
+userid=$(id -u)
 
-echo "NAME in List is ${NAME[0]}" #NAME means Array and [0] means Index
-echo "NAME in List is ${NAME[1]}"
-echo "NAME in List is ${NAME[@]}"
+#(id -u) gives you the user value 
+
+if ( $userid -ne 0 )
+then 
+    echo "Please run with super user"
+else
+    echo "You're a super user"
+fi
+
+dnf install mysql -y
+
+
 
 
